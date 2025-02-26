2ee3f4b9996e0d6434ffdededfeaf05a7a65bd32
LeboL-moriski <lebom.letsoalo@gmail.com>
2025-02-26 22:24:40 +0200

    pic

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ def set_background(image_file):
     page_bg_img = f"""
     <style>
     .stApp {{
-        background-image: url("https://github.com/LeboL-moriski/Streamlit5/blob/master/Pink%20Black%20Modern%20Outer%20Space%20Presentation.jpg");
+        background-image: url("");
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
@@ -50,14 +50,14 @@ def set_background(image_file):
 import base64
 
 def get_base64_of_image(image_path):
-    with open(image_path, "rb") as img_file:
+    with open(image_path, "rb") as img_file:"C:\Users\Lebogang\Documents\GitHub\Streamlit5\Pink Black Modern Outer Space Presentation.jpg"
         return base64.b64encode(img_file.read()).decode()
 
 # Streamlit page content
 st.title("EDA,Group Info,Predictions")
 
 # Set background only for this page
-image_base64 = get_base64_of_image("https://github.com/LeboL-moriski/Streamlit5/blob/master/Pink%20Black%20Modern%20Outer%20Space%20Presentation.jpg")
+image_base64 = get_base64_of_image("C:\Users\Lebogang\Documents\GitHub\Streamlit5\Pink Black Modern Outer Space Presentation.jpg")
 set_background(image_base64)
 
 st.write("EDA,Group Info,Predictions")
