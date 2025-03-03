86b368a3c3c2e82bae75b22eee2121945978381c
Ontha-M <onthatilemoloi9@gmail.com>
2025-03-03 15:03:36 +0200

    Project overview page
    Description of the project

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 
 # Define the custom CSS for the background image
-background_image_url = "https://cdn.pixabay.com/photo/2022/07/06/16/25/beautiful-7305547_1280.jpg"  # Replace this with your image URL
+#background_image_url = "https://cdn.pixabay.com/photo/2022/07/06/16/25/beautiful-7305547_1280.jpg"  # Replace this with your image URL
 
 # Inject the custom CSS into the Streamlit app
 st.markdown(
     f"""
     <style>
         body {{
-            background-image: url({https://cdn.pixabay.com/photo/2022/07/06/16/25/beautiful-7305547_1280.jpg});
+            background-image: url({"https://cdn.pixabay.com/photo/2022/07/06/16/25/beautiful-7305547_1280.jpg"});
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
@@ -37,10 +37,27 @@ st.title("📌 The Recommender System App")
 st.sidebar.title("Navigation")
 
 # Sidebar navigation
-page = st.sidebar.radio("Go to", ["Home", "EDA", "Group Info", "Predictions"])
+page = st.sidebar.radio("Go to", ["Home", "Project Overview", "EDA", "Group Info", "Predictions"])
 
 if page == "Home":
     st.write("Welcome to the Recommender System App!")
+
+elif page == "Project Overview":
+    # Main Title
+    st.title("Project Overview")
+
+    # Project Description
+    st.write("This project focuses on developing a collaborative and content-based recommender system tailored for a collection of anime titles.")
+
+    # Problem Statement
+    st.subheader("Problem Statement")
+    st.write("Develop a recommender system combining collaborative and content-based approaches to predict users' ratings for unseen anime based on past preferences.")
+
+    # Objectives
+    st.subheader("Objectives")
+    st.write("Develop a hybrid recommender system that combines collaborative filtering and content-based techniques to accurately predict how users will rate anime titles they have not yet watched, based on their historical viewing preferences and anime characteristics.")
+
+
 
 elif page == "EDA":
     st.write("## Exploratory Data Analysis")
